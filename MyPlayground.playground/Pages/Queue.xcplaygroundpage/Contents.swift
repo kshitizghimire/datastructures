4776c9a8eb448885aa7ee132b86b9a6e390d291a
Tiz <kshitiz.ghimrie@gmail.com>
2017-08-13 14:24:58 +1000

    stack and queue

---
+++ MyPlayground.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,82 @@
+
+//MARK: Node
+class Node<T> {
+    let value: T
+    var next: Node?
+    
+    //MARK: Lifecycle
+    init(value: T, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+    
+}
+
+//MARK: Queue
+class Queue<T> {
+    var front: Node<T>?
+    var rear: Node<T>?
+    
+    public func enqueue(value: T) {
+        let newRear = Node(value: value, next: nil)
+        if rear == nil {
+            front = newRear
+            rear = newRear
+        } else {
+            rear?.next = newRear
+            rear = newRear
+        }
+        
+    }
+    
+    public func dequeue() -> T? {
+        if let front = front {
+            let currentFront = front
+            self.front = front.next
+            return currentFront.value
+        }
+        return nil
+    }
+    
+    public func peek() -> T? {
+        return front?.value
+    }
+    
+}
+
+
+
+let queue = Queue<Int>()
+
+queue.enqueue(value: 4)
+queue.enqueue(value: 3)
+queue.enqueue(value: 21)
+
+
+queue.peek()
+let firstDequeue = queue.dequeue()
+queue.peek()
+let secondDequeue = queue.dequeue()
+queue.peek()
+let thirdDequeue = queue.dequeue()
+
+struct User {
+    let name: String
+    let email: String
+    
+}
+
+
+let user1 = User(name: "User1", email: "mail1@mail.com")
+let user2 = User(name: "User2", email: "mail2@mail.com")
+
+let userQueue = Queue<User>()
+userQueue.enqueue(value: user1)
+userQueue.enqueue(value: user2)
+
+
+userQueue.peek()
+let firstUserDequeue = userQueue.dequeue()
+userQueue.peek()
+let sencondUserDequeue = userQueue.dequeue()
+
---
+++ MyPlayground.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -0,0 +1,75 @@
+import XCTest
+
+//MARK: Node
+class Node<T> {
+    let value: T
+    var next: Node?
+    
+    //MARK: Lifecycle
+    init(value: T, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+    
+}
+
+//MARK: Stack
+class Stack<T> {
+    var top: Node<T>?
+    
+    public func push(value: T) {
+        let oldTop = top
+        top = Node(value: value, next: nil)
+        top?.next = oldTop
+        
+    }
+    
+    public func pop() -> T? {
+        let currentTop = top
+        top = top?.next
+        return currentTop?.value
+    }
+    
+    public func peek() -> T? {
+        return top?.value
+    }
+    
+}
+
+
+
+let stack = Stack<Int>()
+stack.push(value: 2)
+stack.peek()
+stack.push(value: 3)
+
+stack.peek()
+
+let firstPop = stack.pop()
+let secondPop = stack.pop()
+let thirdPop = stack.pop()
+
+
+
+
+struct User {
+    let name: String
+    let email: String
+}
+
+
+let user1 = User(name: "User1", email: "mail1@mail.com")
+let user2 = User(name: "User2", email: "mail2@mail.com")
+
+
+let userStack = Stack<User>()
+userStack.push(value: user1)
+userStack.peek()
+userStack.push(value: user2)
+userStack.peek()
+
+let firstUserPop = userStack.pop()
+
+let sencondUserPop = userStack.pop()
+
+
